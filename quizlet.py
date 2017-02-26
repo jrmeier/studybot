3ab30d9af2cbc70625435bf9a2c007a8eff7c404
Brian <bespinosa1931@gmail.com>
2017-02-25 18:42:01 -0600

    Update quizlet.py

--- quizlet.py
+++ quizlet.py
@@ -7,6 +7,7 @@
 # =============================================================================
 import requests
 import json
+from random import randint
 
 class Quizlet:
 
@@ -21,6 +22,27 @@ class Quizlet:
         #req = requests.get('https://api.quizlet.com/2.0/users/jed_meier',headers)
         return json.loads(req.content)
 
+    def get_terms(self):
+        code = "https://api.quizlet.com/2.0/sets/415/terms"
+        headers = {'Authorization': 'Bearer sVCXymGVgRXkuj94V6NnnTj9RegXZJ7x8A6Je68Z'}
+        req = requests.get(code,headers=headers)
+        #req = requests.get('https://api.quizlet.com/2.0/users/jed_meier',headers)
+        return json.loads(req.content)
+
+    def json_array(self):
+        code = "https://api.quizlet.com/2.0/sets/415/terms"
+        headers = {'Authorization': 'Bearer sVCXymGVgRXkuj94V6NnnTj9RegXZJ7x8A6Je68Z'}
+        req = requests.get(code,headers=headers)
+        #req = requests.get('https://api.quizlet.com/2.0/users/jed_meier',headers)
+        jsonArray = json.loads(req.content)
+
+        return jsonArray[randint(0, 50)]
+
 if __name__ == '__main__':
     qz = Quizlet("Brian_Espinosa854")
-    print qz.get_sets()
+ #   print qz.get_sets()
+ #   print qz.get_terms()
+    currentCard=qz.json_array()
+
+    print currentCard
+    print (currentCard['definition'])
